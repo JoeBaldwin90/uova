7d8704139ec575a2f58be1c3e1f5a0250e46d9d9
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-08-14 16:29:03 +0100

    Hook up Contentful API using fetch

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello world")
+// Contentful API details
+const spaceId = "28s4t9brspwb"
+const environmentId = "master"
+const accessToken = "ea1fM7QlXrHQKmP8PNdmG-OYEgExAisGOWq49w4EObo"
+
+const url = `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}`
+
+const grabData = function () {
+  // Fetch & parse json data
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+  	.catch(error => {
+  		console.log("error")
+  })
+}
+
+grabData()
