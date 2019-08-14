2e20a6e8b84eecbcb331b83ceffd44691b123fd1
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-08-14 16:41:05 +0100

    Return Contentful data so it can be used

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ const accessToken = "ea1fM7QlXrHQKmP8PNdmG-OYEgExAisGOWq49w4EObo"
 const url = `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}`
 
 const grabData = function () {
-  // Fetch & parse json data
-  fetch(url)
+  // Fetch, parse and return contentful data
+  return fetch(url)
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      return data
     })
   	.catch(error => {
   		console.log("error")
   })
 }
 
-grabData()
+grabData().then(data => {
+  // Do something with the data
+  console.log(data)
+})
