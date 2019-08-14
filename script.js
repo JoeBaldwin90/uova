777fbaa91d79412b20aa14da1097c95645532137
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-08-14 18:39:34 +0100

    Pull in image URL from contentful's data to use in the injected HTML

--- script.js
+++ script.js
@@ -12,8 +12,31 @@ const grabData = function () {
   return fetch(url)
     .then(response => response.json())
     .then(data => {
+      // Store assets contained in the data
+      const assets = data.includes.Asset
+
       // Make data more simple to handle by looping over each item in the data
       return data.items.map(item => {
+
+        // Set default image url
+        let imageUrl = "assets/image5.jpg"
+
+        // Store image ID from data item
+        const imageId = item.fields.image.sys.id
+
+        // Return the object within the Asset data array if asset ID == image ID
+        const imageData = assets.find(asset => {
+          return asset.sys.id == imageId
+        })
+
+        // If the ID's match, use the returned object to grab the image url
+        if (imageData) {
+          imageUrl = imageData.fields.file.url
+        }
+
+        // Set custom data for item.fields.image using the url grabbed from assets
+        item.fields.image = imageUrl
+
         // Then create new array of objects for the fields data in each item
         return item.fields
       })
@@ -25,14 +48,15 @@ const grabData = function () {
 
 grabData().then(data => {
   // Do something with the data
-  console.log(data)
+
   // Remove loader
   sectionTag.innerHTML = ""
 
+  // Build HTML structure and inject JSON data as content
   data.forEach(item => {
     sectionTag.innerHTML = sectionTag.innerHTML + `
     <div class="item">
-      <img src="image1.jpg" />
+      <img src="${item.image}" />
       <div class="title">
         <p>${item.title}</p>
         <p>${item.price}</p>
